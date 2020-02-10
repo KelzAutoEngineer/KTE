26cc5fc04f0f558fdbaa21170ed87cd654e0f52a
Maimane <tmaimakm@unisa.ac.za>
2020-02-10 10:54:14 +0200

    Initial commit

---
+++ Construction/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('projects/', views.projects, name='projects'),
+    path('projects-single/', views.project, name='projects-single'),
+    path('news/', views.news, name='news'),
+    path('news-left/', views.news2, name='news-left'),
+    path('projects/gallery/', views.gallery, name='gallery'),
+    path('projects/gallery2/', views.gallery2, name='gallery2'),
+]
---
+++ Construction/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'Construction/home.html')
+
+
+def about(request):
+    return render(request, 'Construction/about.html', {'title': 'About'})
+
+
+def services(request):
+    return render(request, 'Construction/services.html')
+
+
+def contact(request):
+    return render(request, 'Construction/contact.html', {'title': 'Contact'})
+
+
+def faq(request):
+    return render(request, 'Construction/faq.html', {'title': 'FAQ'})
+
+
+def pricing(request):
+    return render(request, 'Construction/pricing.html', {'title': 'Pricing'})
+
+
+def testimonials(request):
+    return render(request, 'Construction/testimonials.html', {'title': 'Testimonials'})
+
+
+def projects(request):
+    return render(request, 'Construction/projects.html', {'title': 'Projects'})
+
+
+def project(request):
+    return render(request, 'Construction/projects-single.html', {'title': 'Projects-Single'})
+
+
+def news(request):
+    return render(request, 'Construction/news-single.html', {'title': 'News'})
+
+
+def news2(request):
+    return render(request, 'Construction/news-left-sidebar.html', {'title': 'News2'})
+
+
+def gallery(request):
+    return render(request, 'Construction/comm_gallery.html', {'title': 'Commercial Gallery'})
+
+
+def gallery2(request):
+    return render(request, 'Construction/private_gallery.html', {'title': 'Private-Dwellings Gallery'})
